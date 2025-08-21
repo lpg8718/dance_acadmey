6c3d94fe6a1e75e55bb2cf1881535bc5f3ed3af9
Lalit patidar <lalitpatidar23102002@gmail.com>
2025-08-21 23:18:31 +0530

    Fix static files for production

--- my1/settings.py
+++ my1/settings.py
@@ -145,3 +145,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
